0bcb522622254a8e2013138b13da0252ad8ae0ea
tekkac <tarik@carbonable.io>
2024-11-22 17:37:41 +0100

    🍱 add verifier and prover keys for compiled circuit

--- cairo/scripts/starknetdeclare.sh
+++ cairo/scripts/starknetdeclare.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 source ../.env
-starkli declare --estimate-only target/dev/zkmint_Groth16VerifierBN254.contract_class.json --keystore-password $KEYSTORE_PASSWORD --compiler-version 2.8.2
-starkli declare --estimate-only target/dev/zkmint_ZKMint.contract_class.json --keystore-password $KEYSTORE_PASSWORD --compiler-version 2.8.2
+starkli declare --watch target/dev/zkmint_Groth16VerifierBN254.contract_class.json --keystore-password $KEYSTORE_PASSWORD --compiler-version 2.8.2
+starkli declare --watch target/dev/zkmint_ZKMint.contract_class.json --keystore-password $KEYSTORE_PASSWORD --compiler-version 2.8.2
